1664a0f3f12208690c3b2d52362e041eb90da969
SichelBichel <lsiegenthaler79@gmail.com>
2025-05-10 15:24:00 +0200

    hjj

---
+++ RCP/HostCard.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace RCP
+{
+    partial class HostCard
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            panel1 = new Panel();
+            label2 = new Label();
+            label3 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(31, 5);
+            label1.Margin = new Padding(3, 5, 3, 5);
+            label1.Name = "label1";
+            label1.Padding = new Padding(0, 0, 0, 5);
+            label1.Size = new Size(45, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Device:";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(15, 51);
+            button1.Name = "button1";
+            button1.Size = new Size(86, 60);
+            button1.TabIndex = 1;
+            button1.Text = "Start Device";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(126, 51);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(79, 60);
+            panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(53, 35);
+            label2.Margin = new Padding(3, 5, 3, 5);
+            label2.Name = "label2";
+            label2.Padding = new Padding(0, 0, 0, 5);
+            label2.Size = new Size(20, 20);
+            label2.TabIndex = 1;
+            label2.Text = "IP:";
+            label2.Click += label2_Click_1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 35);
+            label3.Margin = new Padding(3, 5, 3, 5);
+            label3.Name = "label3";
+            label3.Padding = new Padding(0, 0, 0, 5);
+            label3.Size = new Size(0, 20);
+            label3.TabIndex = 2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Impact", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(15, 15);
+            label5.Name = "label5";
+            label5.Size = new Size(90, 20);
+            label5.TabIndex = 3;
+            label5.Text = "ServerName";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Impact", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Red;
+            label6.Location = new Point(245, 15);
+            label6.Name = "label6";
+            label6.Size = new Size(28, 20);
+            label6.TabIndex = 4;
+            label6.Text = "Off";
+            // 
+            // HostCard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(panel1);
+            Controls.Add(button1);
+            Name = "HostCard";
+            Size = new Size(298, 132);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Panel panel1;
+        private Label label3;
+        private Label label2;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ RCP/HostCard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RCP
+{
+    public partial class HostCard : UserControl
+    {
+        public HostCard()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
